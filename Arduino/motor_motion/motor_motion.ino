bd9b492d45bdced0c37f67f091234dee847abb17
Lin Huang Ting <138973404+linB110@users.noreply.github.com>
2025-12-10 20:51:08 +0800

    add FSM, turning

--- Arduino/motor_motion/motor_motion.ino
+++ Arduino/motor_motion/motor_motion.ino
@@ -14,6 +14,7 @@ const unsigned long Motion_control_period = 50;
 // base RPM to drive motor
 volatile int base_pwm = 85;
 
+// types od traffic signs
 enum TrafficSigns{
   SIGN_AHEAD_ONLY,
   SIGN_GENERAL_CAUTION,
@@ -25,6 +26,21 @@ enum TrafficSigns{
   SIGN_TURN_RIGHT,
   SIGN_UNKNOWN
 };
+
+// types of motio modes
+enum MotionMode {
+  MODE_FORWARD,
+  MODE_BACKWARD,
+  MODE_STOP,
+  MODE_TURN_LEFT,
+  MODE_TURN_RIGHT
+};
+
+volatile MotionMode motion_mode = MODE_FORWARD;
+volatile MotionMode previous_mode = MODE_FORWARD;
+
+const unsigned long NO_SIGN_TIMEOUT_MS = 7000;
+volatile unsigned long last_sign_ts = 0;
 
 TrafficSigns classify_sign(const String& s) 
 {
@@ -56,30 +72,29 @@ void motion_callback(const std_msgs::String& msg)
 {
   TrafficSigns sign = classify_sign(msg.data);
 
-  
+  last_sign_ts = millis(); 
   
   switch (sign) {
-
     case SIGN_STOP:
+      motion_mode = MODE_STOP;
       base_pwm = 0;
-      stop_motors();
       break;
 
     case SIGN_TURN_LEFT:
-      turn_left();
+      motion_mode = MODE_TURN_LEFT;
       break;
 
     case SIGN_TURN_RIGHT:
-      turn_right();
+      motion_mode = MODE_TURN_RIGHT;
       break;
 
     case SIGN_SPEED_LIMIT:
-      // parse speed value
       base_pwm = parse_speed_limit(msg.data);
+      motion_mode = MODE_FORWARD;
       break;
 
     default:
-      move_forward();
+      motion_mode = MODE_FORWARD;
       break;
   }
 }
@@ -88,32 +103,56 @@ ros::Subscriber<std_msgs::String> sub("valid_motion", motion_callback);
 
 void motion_control_ISR()
 {
+  if (motion_mode != previous_mode)
+    reset_ticks();
+  
   long r = read_right_pulse();
   long l = read_left_pulse();
 
-  int pwmR = 0, pwmL = 0;
-  const int max_comp = 80;     
+  const int max_comp = 80;
   const float Kp = 0.5f;
   const int ticks_tolerance = 5;
 
-  long error = r - l;
+  int pwmR = 0, pwmL = 0;
 
-  if (abs(error) <= ticks_tolerance) error = 0;           
-  int comp = (int)(Kp * error);
-  comp = constrain(comp, -max_comp, max_comp);
+  switch (motion_mode) {
+
+    case MODE_STOP:
+      pwmL = 0;
+      pwmR = 0;
+      break;
+
+    case MODE_FORWARD: {
+      long error = r - l;
+      if (abs(error) <= ticks_tolerance) error = 0;
+
+      int comp = (int)(Kp * error);
+      comp = constrain(comp, -max_comp, max_comp);
+
+      int left  = base_pwm + comp;
+      int right = base_pwm - comp;
+
+      left  = constrain(left,  0, 255);
+      right = constrain(right, 0, 255);
+
+      pwmL = left;
+      pwmR = right;
+      motor_control(pwmR, IN1, IN2, ENA);
+       motor_control(pwmL, IN3, IN4, ENB);
   
-  int left  = base_pwm + comp;
-  int right = base_pwm - comp;
+      break;
+    }
 
-  left  = constrain(left,  0, 255);
-  right = constrain(right, 0, 255);
+    case MODE_TURN_LEFT:
+      turn_left(90, 15);
+      break;
 
-  pwmL = left; 
-  pwmR = right;
-
-  motor_control(pwmR, IN1, IN2, ENA);
-  motor_control(pwmL, IN3, IN4, ENB);
+    case MODE_TURN_RIGHT:
+      turn_right(90, 15);
+      break;
+  }
 }
+
 
 
 void setup() 
@@ -131,8 +170,12 @@ void setup()
 void loop()
 {
   nh.spinOnce();
-  int l  = read_left_rpm();
-  int r = read_right_rpm();
+
+  unsigned long now = millis();
+  if (now - last_sign_ts > NO_SIGN_TIMEOUT_MS) {
+    motion_mode = MODE_FORWARD;   
+    base_pwm = 85;                
+  }
 
   delay(100);
 }
